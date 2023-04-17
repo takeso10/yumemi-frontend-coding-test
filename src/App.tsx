440962664ea6512b3a814a8eeda13abe58be5692
takeso10 <sotatake7716@gmail.com>
2023-04-17 15:56:31 +0900

    タイトル

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,12 @@ import './App.scss'
 import Main from './components/Main'
 
 function App() {
-
   return (
     <div className="App">
       <div className="header">
         <h1>都道府県別人口推移</h1>
       </div>
-      <Main/>
+      <Main />
     </div>
   )
 }
