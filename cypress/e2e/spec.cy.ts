4b21b01194886cfe015e772574e59b4f6af7ed04
takeso10 <sotatake7716@gmail.com>
2023-04-19 11:49:29 +0900

    グラフ表示

---
+++ cypress/e2e/spec.cy.ts
@@ -0,0 +1,10 @@
+describe('template spec', () => {
+  it('passes', () => {
+    cy.visit('https://yumemi-frontend-coding-test-wheat.vercel.app/')
+    cy.get('[value=3]').should('have.id', '岩手県')
+    cy.get('[value=3]').check({ force: true })
+    cy.get('.highcharts-graph').should('be.visible')
+    cy.get('[id="年少人口"]').check({ force: true })
+    cy.get('.highcharts-title').should('have.text', '都道府県別年少人口データ')
+  })
+})
