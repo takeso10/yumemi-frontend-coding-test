d0a2a69dc515324921e69cdce8580d801d2d5590
takeso10 <sotatake7716@gmail.com>
2023-04-21 17:14:30 +0900

    テストコード

--- cypress.config.ts
+++ cypress.config.ts
@@ -2,8 +2,6 @@ import { defineConfig } from 'cypress'
 
 export default defineConfig({
   e2e: {
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
-    },
+    baseUrl:"https://yumemi-frontend-coding-test-wheat.vercel.app/"
   },
 })
