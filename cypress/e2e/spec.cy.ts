f579939a27d307cbb33df7103a27e0f125dd6349
takeso10 <sotatake7716@gmail.com>
2023-04-23 11:01:28 +0900

    環境変数

--- cypress/e2e/spec.cy.ts
+++ cypress/e2e/spec.cy.ts
@@ -1,8 +1,8 @@
 describe('template spec', () => {
   it('passes', () => {
     cy.visit('https://yumemi-frontend-coding-test-wheat.vercel.app/')
-    cy.get('[value=3]').should('have.id', '岩手県')
-    cy.get('[value=3]').check({ force: true })
+    cy.get('[id="岩手県"]').should('have.value', '3')
+    cy.get('[id="岩手県"]').check({ force: true })
     cy.get('.highcharts-graph').should('be.visible')
     cy.get('.highcharts-title').should('have.text', '都道府県別総人口データ')
     cy.get('[id="年少人口"]').check({ force: true })
