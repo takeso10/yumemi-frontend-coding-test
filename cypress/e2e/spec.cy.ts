71a6cb7bce02a2703eabaefce2c9e050a6a49d4c
takeso10 <sotatake7716@gmail.com>
2023-04-23 10:56:16 +0900

    e2eテスト

--- cypress/e2e/spec.cy.ts
+++ cypress/e2e/spec.cy.ts
@@ -4,6 +4,7 @@ describe('template spec', () => {
     cy.get('[value=3]').should('have.id', '岩手県')
     cy.get('[value=3]').check({ force: true })
     cy.get('.highcharts-graph').should('be.visible')
+    cy.get('.highcharts-title').should('have.text', '都道府県別総人口データ')
     cy.get('[id="年少人口"]').check({ force: true })
     cy.get('.highcharts-title').should('have.text', '都道府県別年少人口データ')
   })
