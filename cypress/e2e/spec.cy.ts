ae53c5ec70961aee652404d2e286d4daf898ac0c
takeso10 <sotatake7716@gmail.com>
2023-04-24 14:37:49 +0900

    e2eテスト

--- cypress/e2e/spec.cy.ts
+++ cypress/e2e/spec.cy.ts
@@ -1,5 +1,5 @@
-describe('template spec', () => {
-  it('passes', () => {
+describe('check test', () => {
+  it('select prefecture, view gragh ', () => {
     cy.visit('https://yumemi-frontend-coding-test-wheat.vercel.app/')
     cy.get('[id="岩手県"]').should('have.value', '3')
     cy.get('[id="岩手県"]').check({ force: true })
